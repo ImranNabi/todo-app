090619820239f8fe055946a729f427a502e35054
ImranNabi <imran.nabi2005@gmail.com>
2025-07-06 14:34:19 +0530

    cors url   updated

--- TODO/todo_backend/server.js
+++ TODO/todo_backend/server.js
@@ -4,21 +4,24 @@ const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const todoRoutes = require('./routes/todos');
 require('dotenv').config();
-const JWT_SECRET = process.env.JWT_SECRET;
-const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+
 const app = express();
-app.use(cors({ origin: ['http://localhost:3000','https://todo-app-jet-two-66.vercel.app/'] }));
+
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://todo-app-jet-two-66.vercel.app'
+  ],
+  credentials: true
+}));
+app.options('*', cors());
+
 app.use(express.json());
-// console.log(process.env.MONGODB_URI);
-// mongoose.connect('mongodb+srv://imrannabi2005:Imran@kce786@cluster0.s2pq9rc.mongodb.net/todo-app?retryWrites=true&w=majority')
-//     .then(() => console.log('MongoDB connected'))
-//     .catch(err => console.error('MongoDB connection error:', err));
+
 const connectDB = require('./connectDB');
 connectDB();
-  
+
 app.use('/auth', authRoutes);
 app.use('/todos', todoRoutes);
 
 app.listen(5000, () => console.log('Server listening on port: 5000'));
-
-module.exports = app;
