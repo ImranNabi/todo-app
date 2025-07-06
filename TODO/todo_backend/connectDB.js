82e7427828b94ea606484fa44262dc02bb615219
ImranNabi <imran.nabi2005@gmail.com>
2025-07-06 14:01:27 +0530

    mongoDB URL updated

---
+++ TODO/todo_backend/connectDB.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const dotenv=require("dotenv");
+dotenv.config();
+const connectDB = async () => {
+  try {
+    const uri = process.env.MONGODB_URI;
+  //  console.log(uri);
+  await mongoose.connect(uri, {
+    connectTimeoutMS: 10000,
+  });
+
+    console.log("MongoDB Atlas Connected Successfully");
+  } catch (err) {
+    console.error("Error connecting to MongoDB Atlas:", err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- TODO/todo_backend/server.js
+++ TODO/todo_backend/server.js
@@ -7,13 +7,15 @@ require('dotenv').config();
 const JWT_SECRET = process.env.JWT_SECRET;
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: ['http://localhost:3000','https://todo-gl6pfnsoq-imrans-projects-25659def.vercel.app/login'] }));
 app.use(express.json());
-
-mongoose.connect('mongodb://localhost:27017/list')
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
-
+// console.log(process.env.MONGODB_URI);
+// mongoose.connect('mongodb+srv://imrannabi2005:Imran@kce786@cluster0.s2pq9rc.mongodb.net/todo-app?retryWrites=true&w=majority')
+//     .then(() => console.log('MongoDB connected'))
+//     .catch(err => console.error('MongoDB connection error:', err));
+const connectDB = require('./connectDB');
+connectDB();
+  
 app.use('/auth', authRoutes);
 app.use('/todos', todoRoutes);
 
